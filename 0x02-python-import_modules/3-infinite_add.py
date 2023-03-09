57f69cc37b161526b24dfec8838255ca12e74514
Leac1m <obemichael10@gmail.com>
2023-03-09 15:44:48 +0100

    adds command line arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    
+    len = len(sys.argv)
+    if len == 1:
+        print("0")
+    else:
+        sum = 0
+        for i in range(len):
+            if i == 0:
+                continue
+            sum += int(sys.argv[i])
+        print(sum)
