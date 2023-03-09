3bac4fea24fd0eed2f5124f0827e72855027aa80
Leac1m <obemichael10@gmail.com>
2023-03-09 15:27:35 +0100

    print list of arguments entered

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
     len = len(sys.argv)
     if len == 1:
         print("0 arguments.")
+    elif len == 2:
+        print("{} arguments".format(len - 1))
+        print("{}: {}".format(len -1, sys.argv[len - 1]))
     else:
+        print("{} arguments".format(len - 1))
         for i in range(len):
             if i == 0:
                 continue
