ddddc3f1e58feb5db6c6612deb3dfcba97323b82
Leac1m <obemichael10@gmail.com>
2023-03-12 13:36:01 -0700

    call

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     import calculator_1 as cal
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
@@ -27,4 +27,4 @@ if __name__ == "__main__":
         print(f"{a} / {b} = {result}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
