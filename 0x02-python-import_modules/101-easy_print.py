c68bd3da0c2705dee7fe1393df16aa2831ba22bb
Leac1m <obemichael10@gmail.com>
2023-03-12 13:44:02 -0700

    easy_print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    "prints with normal functions"
-    __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
