5e26774d6d5fed2c7f845988e55ab14a7e970511
Leac1m <obemichael10@gmail.com>
2023-03-12 13:16:47 -0700

    fixed bugs

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """print the sum, difference, product and division of 10 and 5."""
-
+    """Print the sum, difference, product, quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
